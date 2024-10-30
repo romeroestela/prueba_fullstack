79e66c8b43923a4fedc25cd3b0ceb951fbe7a8ef
estela.romero.edu <romeroestela3@gmail.com>
2024-10-30 21:13:12 +0100

    Corregir error en la funcion getEntriesWithoutSensitiveInfo

--- back_end/src/servicies/operationServicies.ts
+++ back_end/src/servicies/operationServicies.ts
@@ -6,13 +6,15 @@ const operations: OperationEntry[] = operationData as OperationEntry[]
 export const getEntries = (): OperationEntry[] => operations
 
 export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
-    return operations.map(({type, amount, price, market_name, client_name}) => {
-        type
-        amount
-        price
-        market_name
-        client_name
-    })
+  return operations.map(({ type, amount, price, market_name, client_name }) => {
+    return {
+      type,
+      amount,
+      price,
+      market_name,
+      client_name
+    }
+  })
 }
 
 export const addOperation = (): undefined => undefined
