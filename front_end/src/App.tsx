8ea24947898e00f7edbd0f5966b1bdc1ae3dde0f
estela.romero.edu <romeroestela3@gmail.com>
2024-11-04 16:26:02 +0100

    Separar componente Table y crear archiv types.d.ts

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,37 +1,8 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import Form from './components/form';
-
-interface Operation{
-  id: number,
-  marketer_id: number,
-  client_id: number,
-  type: 'buy' | 'sell',
-  amount: number,
-  price: number,
-  marketer_name: string,
-  client_name: string
-}
+import Form from './components/Form';
+import Table from './components/Table';
 
 function App() {
-  //Estado para almacenar la lista de operaciones
-  const [operations, setOperations] = useState<Operation[]>([]);
-
-  useEffect(() => {
-    const fetchOperations = async() => {
-      try {
-      //Solicitud GET al backend para obeter operaciones
-        const response = await fetch('http://localhost:3000/api/operations');
-        const data = await response.json();
-        setOperations(data); //Almacenamos las operaciones en el estado
-      } catch (error) {
-        console.error('Error al cargar las operaciones:', error);
-      }
-    };
-
-    fetchOperations();
-  }, []);
-
   return (
     <main>
       <aside>
@@ -41,33 +12,7 @@ function App() {
 
       <section>
         <h2>Lista de operaciones</h2>
-        {operations.length === 0 ? (
-          <p>Aun no hay operaciones</p>
-        ) : (
-          <table>
-            <thead>
-              <tr>
-                <th>Compra o Venta</th>
-                <th>Comercializadora</th>
-                <th>Cliente</th>
-                <th>Cantidad de gas</th>
-                <th>Precio</th>
-              </tr>
-            </thead>
-            <tbody>
-              {operations.map((operation) => (
-                <tr key={operation.id}>
-                  <td>{operation.type}</td>
-                  <td>{operation.marketer_name}</td>
-                  <td>{operation.client_name}</td>
-                  <td>{operation.amount}</td>
-                  <td>{operation.price}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-        )}
+        <Table />
       </section>
     </main>
   );
---
+++ front_end/src/components/Table.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import { Operation } from '../types';
+const Table = () => {
+  //Estado para almacenar la lista de operaciones
+  const [operations, setOperations] = useState<Operation[]>([]);
+  useEffect(() => {
+    const fetchOperations = async() => {
+      try {
+      //Solicitud GET al backend para obeter operaciones
+        const response = await fetch('http://localhost:3000/api/operations');
+        const data = await response.json();
+        setOperations(data); //Almacenamos las operaciones en el estado
+      } catch (error) {
+        console.error('Error al cargar las operaciones:', error);
+      }
+    };
+    fetchOperations();
+  }, []);
+  return(
+    <>
+      {operations.length === 0 ? (
+        <p>Aun no hay operaciones</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>Compra o Venta</th>
+              <th>Comercializadora</th>
+              <th>Cliente</th>
+              <th>Cantidad de gas</th>
+              <th>Precio</th>
+            </tr>
+          </thead>
+          <tbody>
+            {operations.map((operation) => (
+              <tr key={operation.id}>
+                <td>{operation.type}</td>
+                <td>{operation.marketer_name}</td>
+                <td>{operation.client_name}</td>
+                <td>{operation.amount}</td>
+                <td>{operation.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+};
+export default Table;
