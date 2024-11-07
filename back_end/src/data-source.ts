5c3d7f2b661a852a5ba480b2265a6acbee79c36b
estela.romero.edu <romeroestela3@gmail.com>
2024-11-07 11:06:37 +0100

    Configurar la conexión a la base de datos y crear entidades iniciales para operaciones y comercializadoras

---
+++ back_end/src/data-source.ts
@@ -0,0 +1,19 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Marketer } from "./entity/Marketer";
+import { Operation } from "./entity/Operation";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",      // Dirección del host de tu base de datos
+  port: 5432,             // Puerto estándar de PostgreSQL
+  username: "postgres",   // Tu usuario de PostgreSQL
+  password: "yourpassword",  // Contraseña de tu usuario de PostgreSQL
+  database: "mydatabase",   // El nombre de tu base de datos
+  entities: [
+    Marketer, // Entidades del proyecto
+    Operation            
+  ],
+  synchronize: true,      // Crea las tablas automáticamente
+  logging: true,          // Activar logging para ver las consultas SQL
+});
--- marketers.sql
+++
@@ -1,9 +0,0 @@
--- Inserta datos en la tabla marketers
-INSERT INTO marketers (name) 
-VALUES 
-    ('Comercializadora 1'),
-    ('Comercializadora 2'),
-    ('Comercializadora 3'),
-    ('Comercializadora 4'),
-    ('Comercializadora 5');
-
