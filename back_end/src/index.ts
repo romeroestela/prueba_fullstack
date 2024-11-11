38475bffe4067c579e25d0fbea0588a79bfe8215
estela.romero.edu <romeroestela3@gmail.com>
2024-11-11 12:38:23 +0100

    Añadir comentarios en `index.ts`

--- back_end/src/index.ts
+++ back_end/src/index.ts
@@ -5,19 +5,21 @@ import { AppDataSource } from './data-source'
 const app = express()
 const PORT = 3000
 
-// Modificación: Inicializa la conexión a la BD antes de iniciar el servidor
+// Conecta la conexión a la BD antes de iniciar el servidor
 AppDataSource.initialize()
   .then(() => {
     console.log('Conectado a la base de datos')
-    app.use(express.json())
+    app.use(express.json()) // Configura express para interpretar JSON en las solicitudes
 
+    // Configura las cabeceras para permitir solicitudes desde cualquier origen (CORS)
     app.use((_req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Headers', 'Content-Type')
-      res.header('Access-Control-Allow-Methods', 'GET, POST')
+      res.header('Access-Control-Allow-Origin', '*') // Permitir acceso desde cualquier dominio. No es seguro, pero es útil para esta situación.  
+      res.header('Access-Control-Allow-Headers', 'Content-Type') // Permitir cabecera de tipo JSON
+      res.header('Access-Control-Allow-Methods', 'GET, POST') // Métodos HTTP permitidos
       next()
     })
 
+    // Define la ruta para las operaciones de la API
     app.use('/api/operations', operationsRouter)
 
     // Inicia el servidor solo después de conectar con éxito a la BD
