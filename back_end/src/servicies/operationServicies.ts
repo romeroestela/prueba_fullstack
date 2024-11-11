bd1e1f1294b979a544f80fc9abca6d70f659aaad
estela.romero.edu <romeroestela3@gmail.com>
2024-11-11 12:22:34 +0100

    Añadir comentarios en `operationsServicies.ts`

--- back_end/src/servicies/operationServicies.ts
+++ back_end/src/servicies/operationServicies.ts
@@ -7,6 +7,7 @@ import { Type } from '../enums'
 const operationRepository = AppDataSource.getRepository(Operations)
 const marketerRepository = AppDataSource.getRepository(Marketer)
 
+// Obtiene todas las operaciones sin mostrar datos sensibles (los clientes o comercializadoras desconocidos se muestran como "Desconocidos" esto puede suceder si se ha eliminado la comercializadora, ya que sigue guardandose la operación)
 export const getEntriesWithoutSensitiveInfo = async (): Promise<NonSensitiveInfoEntry[]> => {
   const operations = await operationRepository.find({
     relations: ['marketer', 'client']
@@ -28,27 +29,33 @@ class ValidationError extends Error {
   constructor (message: string) {
     super(message)
     this.name = 'ValidationError'
-    this.statusCode = 400 // Código de error para solicitudes incorrectas
+    this.statusCode = 400
   }
 }
 
+// Agrega una nueva operación, validando los IDs y que los datos existan en la base de datos
 export const addOperation = async (newOperationEntry: newOperationEntry): Promise<OperationEntry> => {
   const { marketer_id, client_id, type, amount, price } = newOperationEntry
 
   try {
+    // Validar que la comercializadora y el cliente no peden ser el mismo
     if (marketer_id === client_id) {
       throw new ValidationError('El ID de tu comercializadora no puede ser el mismo que el del cliente')
     }
+
+    // Verifica que la comercializadora exista
     const marketer = await marketerRepository.findOne({ where: { id: marketer_id } })
     if (marketer == null) {
       throw new ValidationError(`Comercializadora con ID ${marketer_id} no encontrada`)
     }
 
+    // Verifica que el cliente exista
     const client = await marketerRepository.findOne({ where: { id: client_id } })
     if (client == null) {
       throw new ValidationError(`Cliente con ID ${client_id} no encontrado`)
     }
 
+    // Crea y guarda la operación con los datos proporcionados
     const newOperation = operationRepository.create({
       marketer,
       client,
@@ -59,6 +66,7 @@ export const addOperation = async (newOperationEntry: newOperationEntry): Promis
 
     const savedOperation = await operationRepository.save(newOperation)
 
+    // Retorna la operación guardada con los datos organizados
     return {
       id: savedOperation.id,
       marketer_id: savedOperation.marketer.id,
@@ -73,7 +81,7 @@ export const addOperation = async (newOperationEntry: newOperationEntry): Promis
     if (error instanceof ValidationError) {
       throw error // Los errores de validación se envían al controlador para una respuesta específica
     } else {
-      throw new Error('Error al guardar la operación')
+      throw new Error('Error al guardar la operación') // Error génerico si la operación falla
     }
   }
 }
